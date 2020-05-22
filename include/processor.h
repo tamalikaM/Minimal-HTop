1e76d0146de366e5a1480027cd6bd30d13d1ed33
tamalikaM <tamalika0101@gmail.com>
2020-05-22 22:06:46 +0000

    improvement

--- include/processor.h
+++ include/processor.h
@@ -7,9 +7,7 @@ class Processor {
 
   // TODO: Declare any necessary private members
  private:
-    float prev_user_, prev_nice_, prev_system_,prev_idle_,prev_iowait_,
-    prev_irq_,prev_softirq_,prev_steal_,prev_guest_,prev_guestnice_,prev_non_idle_,prev_total_ ;
+    float prev_idle_, prev_total_ ;
 };
-
 
 #endif
